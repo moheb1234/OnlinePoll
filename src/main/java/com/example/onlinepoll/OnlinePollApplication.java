e92bf54f10ef023431c4067eecd9188556af22c6
Mr. X <mohebmoallem1376@gmail.com>
2022-08-18 18:36:10 +0530

    heroku config

--- src/main/java/com/example/onlinepoll/OnlinePollApplication.java
+++ src/main/java/com/example/onlinepoll/OnlinePollApplication.java
@@ -8,17 +8,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@AllArgsConstructor
-public class OnlinePollApplication implements CommandLineRunner {
-    private final UserRepository userRepository;
+//@AllArgsConstructor
+public class OnlinePollApplication  {
+//    private final UserRepository userRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(OnlinePollApplication.class, args);
     }
 
-    @Override
-    public void run(String... args)  {
-        User admin = new User("moheb","moallem","moheb12","moheb123456");
-        userRepository.save(admin);
-    }
+//    @Override
+//    public void run(String... args)  {
+//        User admin = new User("moheb","moallem","moheb12","moheb123456");
+//        userRepository.save(admin);
+//    }
 }
