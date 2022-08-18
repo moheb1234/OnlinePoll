e914e0a5f51c2138ac07bbe2a7aba9a243227b71
Mr. X <mohebmoallem1376@gmail.com>
2022-08-18 18:36:23 +0530

    heroku config

--- src/main/java/com/example/onlinepoll/OnlinePollApplication.java
+++ src/main/java/com/example/onlinepoll/OnlinePollApplication.java
@@ -1,21 +1,16 @@
 package com.example.onlinepoll;
 
-import com.example.onlinepoll.model.User;
-import com.example.onlinepoll.repository.UserRepository;
-import lombok.AllArgsConstructor;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 //@AllArgsConstructor
-public class OnlinePollApplication  {
+public class OnlinePollApplication {
 //    private final UserRepository userRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(OnlinePollApplication.class, args);
     }
-
 //    @Override
 //    public void run(String... args)  {
 //        User admin = new User("moheb","moallem","moheb12","moheb123456");
